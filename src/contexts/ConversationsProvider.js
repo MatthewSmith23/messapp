9b56f66a3852fb609acc185ceb88cb981287ad32
Matthew <smith0223@ymail.com>
2021-01-20 03:04:56 -0500

    sidebar..moving onto main convo area

--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,6 +1,7 @@
 import userEvent from '@testing-library/user-event';
-import React, { useContext } from 'react'
+import React, { useContext, useState} from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
+import { useContacts } from './ContactsProvider'
 
 
 const ConversationsContext = React.createContext()
@@ -11,6 +12,10 @@ export function useConversations() {
 
 export function ConversationsProvider({ children }) {
 const [conversations, setConversations] = useLocalStorage('conversations', [])
+const [selectedConversationIndex, setSelectedConversationIndex] =
+useState(0)
+
+const { contacts } = useContacts()
 
 function createConversation(recipients) {
     setConversations(prevConversations => {
@@ -18,8 +23,29 @@ function createConversation(recipients) {
     })
 }
 
+const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map(recipient => {
+        const contact = contacts.find(contact => {
+            return contact.id === recipient 
+        })
+        const name = (contact && contact.name) || recipient 
+        return { id: recipient, name }
+    })
+    const selected = index === selectedConversationIndex
+    return { ...conversation, recipients, selected }
+})
+
+const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations
+    [selectedConversationIndex],
+    selectConversationIndex: setSelectedConversationIndex,
+     createConversation
+}
+
+
     return (
-        <ConversationsContext.Provider value={{ conversations, createConversation }}>
+        <ConversationsContext.Provider value={value}>
 {children}
 
         </ConversationsContext.Provider>
